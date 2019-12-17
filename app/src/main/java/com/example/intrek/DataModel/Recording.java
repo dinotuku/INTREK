674d550f4b36c374fffd24e093126b1b82d12e18
arthurBricq <arthur.bricq@epfl.ch>
2019-12-17 15:55:38 +0100

    Adding the datamodel

---
+++ app/src/main/java/com/example/intrek/DataModel/Recording.java
@@ -0,0 +1,25 @@
+package com.example.intrek.DataModel;
+
+import java.util.ArrayList;
+
+public class Recording {
+
+    // MARK: - Fields
+
+    private int duration = 0 ; // [s]
+    private ArrayList<Float> HRs = new ArrayList<>();
+
+    // MARK: - Public methods
+
+    public Recording() {
+        // todo
+    }
+
+    public void saveToFirebase() {
+        // todo
+    }
+
+    // MARK: - Private methods
+
+
+}
