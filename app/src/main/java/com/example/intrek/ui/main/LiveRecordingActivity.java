0c07ee2219981e8a126944f1e11ceb778b80708d
abricq <arthur.bricq@epfl.ch>
2019-12-25 16:54:34 +0100

    I added the map with the map API and I retrieved the location using the LocationManager. There are two big steps that i can do now: - show the continuous path on the map - using the location, compute the speed and the distance

--- app/src/main/java/com/example/intrek/ui/main/LiveRecordingActivity.java
+++ app/src/main/java/com/example/intrek/ui/main/LiveRecordingActivity.java
@@ -1,16 +1,24 @@
 package com.example.intrek.ui.main;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
@@ -31,7 +39,7 @@ import java.util.ArrayList;
 
 import static com.example.intrek.R.id.HRPlot;
 
-public class LiveRecordingActivity extends AppCompatActivity {
+public class LiveRecordingActivity extends AppCompatActivity implements LocationListener {
 
     public static final String ACTION_RECEIVE_HEART_RATE = "ACTION_RECEIVE_HEART_RATE";
     public static final String HEART_RATE = "HeartRate";
@@ -41,11 +49,11 @@ public class LiveRecordingActivity extends AppCompatActivity {
     private static final String HR_PLOT_WATCH = "HR from smart watch";
 
     private Chronometer timerTextView;
-    private Button pauseButton ;
+    private Button pauseButton;
     private XYPlot heartRatePlot;
 
-    private long timerValueWhenPaused = 0 ;
-    private boolean isPaused = false ;
+    private long timerValueWhenPaused = 0;
+    private boolean isPaused = false;
     private HeartRateBroadcastReceiver heartRateBroadcastReceiver;
     private int heartRateWatch = 0;
     private ArrayList<Integer> hrDataArrayList = new ArrayList<>();
@@ -62,16 +70,28 @@ public class LiveRecordingActivity extends AppCompatActivity {
         pauseButton = findViewById(R.id.PauseButton);
         heartRatePlot = findViewById(HRPlot);
         configurePlot();
-        xyPlotSeriesList = new XYPlotSeriesList();
-        LineAndPointFormatter formatterWatch = new LineAndPointFormatter(Color.RED, Color.TRANSPARENT, Color.TRANSPARENT, null);
-        formatterWatch.getLinePaint().setStrokeWidth(8);
-        xyPlotSeriesList.initializeSeriesAndAddToList(HR_PLOT_WATCH, MIN_HR, NUMBER_OF_POINTS, formatterWatch);
-        XYSeries HRseries = new SimpleXYSeries(xyPlotSeriesList.getSeriesFromList(HR_PLOT_WATCH), SimpleXYSeries.ArrayFormat.XY_VALS_INTERLEAVED, HR_PLOT_WATCH);
-        heartRatePlot.clear();
-        heartRatePlot.addSeries(HRseries, formatterWatch);
-        heartRatePlot.redraw();
+        setHRPlot();
+
+        // 2. Add location manager
+        Log.e("Test","Test1");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && (checkSelfPermission("android" + "" + ".permission.ACCESS_FINE_LOCATION") == PackageManager.PERMISSION_DENIED || checkSelfPermission("android.permission.ACCESS_COARSE_LOCATION") == PackageManager.PERMISSION_DENIED || checkSelfPermission("android" + "" + ".permission.INTERNET") == PackageManager.PERMISSION_DENIED)) {
+            requestPermissions(new String[]{"android.permission.ACCESS_FINE_LOCATION", "android" + ".permission.ACCESS_COARSE_LOCATION", "android.permission.INTERNET"}, 0);
+        }
+
+        // Acquire a reference to the system Location Manager
+        final LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager != null) {
+            try {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) this);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+            } catch (Exception e) {
+                Log.w("ERROR", "Could not request location updates");
+            }
+        }
 
     }
+
 
     @Override
     protected void onResume() {
@@ -87,6 +107,11 @@ public class LiveRecordingActivity extends AppCompatActivity {
         LocalBroadcastManager.getInstance(this).unregisterReceiver(heartRateBroadcastReceiver);
     }
 
+    public void mapButtonTapped(View view) {
+        // Open a new activity which will show the map
+        Intent startMapIntent = new Intent(LiveRecordingActivity.this, LiveMapActivity.class);
+        startActivity(startMapIntent);
+    }
 
     public void pauseButtonTapped(View view) {
         if (isPaused) {
@@ -127,6 +152,43 @@ public class LiveRecordingActivity extends AppCompatActivity {
         heartRatePlot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.LEFT).setFormat(new DecimalFormat("#"));
         // This line is to force the Axis to be integer
         heartRatePlot.setRangeLabel("Heart rate (bpm)");
+    }
+
+    private void setHRPlot() {
+        xyPlotSeriesList = new XYPlotSeriesList();
+        LineAndPointFormatter formatterWatch = new LineAndPointFormatter(Color.RED, Color.TRANSPARENT, Color.TRANSPARENT, null);
+        formatterWatch.getLinePaint().setStrokeWidth(8);
+        xyPlotSeriesList.initializeSeriesAndAddToList(HR_PLOT_WATCH, MIN_HR, NUMBER_OF_POINTS, formatterWatch);
+        XYSeries HRseries = new SimpleXYSeries(xyPlotSeriesList.getSeriesFromList(HR_PLOT_WATCH), SimpleXYSeries.ArrayFormat.XY_VALS_INTERLEAVED, HR_PLOT_WATCH);
+        heartRatePlot.clear();
+        heartRatePlot.addSeries(HRseries, formatterWatch);
+        heartRatePlot.redraw();
+    }
+
+    // MARK: - methods for LocationListener implementation
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        TextView textViewGPS = findViewById(R.id.pressureTextView);
+        if (textViewGPS != null) textViewGPS.setText("Lat: " + latitude + "\nLon: " + longitude);
+        Log.i("LOCATION","Lat: " + latitude + "\nLon: " + longitude);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 
 
