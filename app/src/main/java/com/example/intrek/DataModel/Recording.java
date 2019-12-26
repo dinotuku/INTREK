74aa2d45af454fa8d0c25c17384f9f12adcd214a
abricq <arthur.bricq@epfl.ch>
2019-12-26 10:41:36 +0100

    Now, I did a better average computation to kill the noise obtained with the GPS values I am working on the analysis of one hike. So far, I measure: - the HR value - the speed - the distance of the hike

--- app/src/main/java/com/example/intrek/DataModel/Recording.java
+++ app/src/main/java/com/example/intrek/DataModel/Recording.java
@@ -1,5 +1,7 @@
 package com.example.intrek.DataModel;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import java.sql.Time;
 import java.util.ArrayList;
 
@@ -7,30 +9,51 @@ public class Recording {
 
     // MARK: - Fields
 
-    // About time
+    ///// Generic information about the hike
+    
     private Time startingTime ;
     private Time endingTime ;
-    // All positions
-    private ArrayList<Double> lats = new ArrayList<>();
-    private ArrayList<Double> longs = new ArrayList<>();
-    // Collected data
-    private ArrayList<Float> HRs = new ArrayList<>();
-
-    // todo : add all the sensors
-    // Comments of the users
     private int grade; // Out of 5
     private String name;
-    
+
+    //// Collected data on the hike to be analysed
+
+    // Arrays for the location and the distance
+    private ArrayList<Long> locationsTimes = new ArrayList<>();
+    private ArrayList<LatLng> locations = new ArrayList<>();
+    private ArrayList<Double> distances = new ArrayList<>();
+
+    // Arrays for the speed
+    private ArrayList<Double> speeds = new ArrayList<>();
+    private ArrayList<Long> speedsTimes = new ArrayList<>();
+
+    // Arrays for the HR
+    private ArrayList<Integer> hrDataArrayList = new ArrayList<>();
+    private ArrayList<Long> hrTimes = new ArrayList<>();
 
     // MARK: - Public methods
+
+    public Recording(ArrayList<Long> locationsTimes, ArrayList<LatLng> locations, ArrayList<Double> distances, ArrayList<Double> speeds, ArrayList<Long> speedsTimes, ArrayList<Integer> hrDataArrayList, ArrayList<Long> hrTimes) {
+        this.locationsTimes = locationsTimes;
+        this.locations = locations;
+        this.distances = distances;
+        this.speeds = speeds;
+        this.speedsTimes = speedsTimes;
+        this.hrDataArrayList = hrDataArrayList;
+        this.hrTimes = hrTimes;
+    }
 
     public Recording() {
         // todo
     }
 
+    // This constructor is to build the statistics
     public void saveToFirebase() {
         // todo
     }
+
+
+
 
     // MARK: - Private methods
 
